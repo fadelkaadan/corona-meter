e08e439ffa1f2511fdcb6b6e207e4ab25327713e
fadelkaadan <fadelkaadan@gmail.com>
2020-04-06 19:21:03 +0200

    Updates

---
+++ precache-manifest.d543cbd1a02c22576deea80317ac398b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e22c31cab53516fa22c203c2ce1ee9d0",
+    "url": "/corona-meter/index.html"
+  },
+  {
+    "revision": "072dded6c76283efd98a",
+    "url": "/corona-meter/static/css/main.9c788c07.chunk.css"
+  },
+  {
+    "revision": "0026dc806cb12cfc7e4a",
+    "url": "/corona-meter/static/js/2.ed34259b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/corona-meter/static/js/2.ed34259b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "072dded6c76283efd98a",
+    "url": "/corona-meter/static/js/main.649ad278.chunk.js"
+  },
+  {
+    "revision": "a310c1e08b1ae5d12c89",
+    "url": "/corona-meter/static/js/runtime-main.6678e17d.js"
+  }
+]);
